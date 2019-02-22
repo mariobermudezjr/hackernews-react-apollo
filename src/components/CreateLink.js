e0e06cfc274c4c22d884846299b9619eaf26647b
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-02-22 15:04:05 -0800

    Implemented Authentication, Mutations and updates to the store

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
+import { FEED_QUERY } from './LinkList';
+
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
     post(description: $description, url: $url) {
@@ -12,24 +14,6 @@ const POST_MUTATION = gql`
     }
   }
 `;
-
-// const POST_MUTATION2 = graphql(gql`mutation PostMutation($description: String!, $url: String!) {
-//     post(description: $description, url: $url) {
-//       id
-//       createdAt
-//       url
-//       description
-//     }
-//   }`, {
-//     options: {
-//       context: {
-//         headers: {
-//             "Authorization": "Bearer __TOKEN__"
-//         }
-//       },
-//       // ... other options
-//     }
-//   })
 
 class CreateLink extends Component {
   state = {
@@ -62,6 +46,14 @@ class CreateLink extends Component {
           mutation={POST_MUTATION}
           variables={{ description, url }}
           onCompleted={() => this.props.history.push('/')}
+          update={(store, { data: { post } }) => {
+            const data = store.readQuery({ query: FEED_QUERY });
+            data.feed.links.unshift(post);
+            store.writeQuery({
+              query: FEED_QUERY,
+              data
+            });
+          }}
         >
           {postMutation => <button onClick={postMutation}>Submit</button>}
         </Mutation>
